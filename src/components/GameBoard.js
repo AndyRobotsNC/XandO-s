f6a4c7833db94f72be486c2ec67d51626c450a84
andy <andyrobotsdroid@gmail.com>
2022-01-04 09:31:55 +0000

    attempting winning logic

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,7 +1,30 @@
 const GameBoard = ({ symbol, setSymbol }) => {
+  const winningCombosX = [
+    "123",
+    "159",
+    "147",
+    "258",
+    "357",
+    "369",
+    "456",
+    "789",
+  ];
+  const winningCombosO = [
+    "123",
+    "159",
+    "147",
+    "258",
+    "357",
+    "369",
+    "456",
+    "789",
+  ];
+
   const addSymbol = (event) => {
     const gridItemClassList = event.target.classList.value.split(" ");
-    console.log(gridItemClassList);
+    const gridNum = event.target.innerHTML;
+    console.log(winningCombosO);
+
     if (
       gridItemClassList.includes("cross") ||
       gridItemClassList.includes("nought")
@@ -9,6 +32,16 @@ const GameBoard = ({ symbol, setSymbol }) => {
     } else if (symbol === "X") {
       event.target.classList.add("cross");
       setSymbol("O");
+
+      winningCombosO.forEach((combo) => {
+        console.log(combo);
+
+        if (combo.includes(gridNum)) {
+          let comboPosition = winningCombosO.indexOf(combo);
+          winningCombosO.splice(comboPosition, 1);
+        }
+        console.log(winningCombosO);
+      });
     } else if (symbol === "O") {
       event.target.classList.add("nought");
       setSymbol("X");
@@ -17,15 +50,33 @@ const GameBoard = ({ symbol, setSymbol }) => {
 
   return (
     <div class="grid-container">
-      <div onMouseDown={addSymbol} class="grid-item"></div>
-      <div onMouseDown={addSymbol} class="grid-item"></div>
-      <div onMouseDown={addSymbol} class="grid-item"></div>
-      <div onMouseDown={addSymbol} class="grid-item"></div>
-      <div onMouseDown={addSymbol} class="grid-item"></div>
-      <div onMouseDown={addSymbol} class="grid-item"></div>
-      <div onMouseDown={addSymbol} class="grid-item"></div>
-      <div onMouseDown={addSymbol} class="grid-item"></div>
-      <div onMouseDown={addSymbol} class="grid-item"></div>
+      <div onMouseDown={addSymbol} class="grid-item">
+        1
+      </div>
+      <div onMouseDown={addSymbol} class="grid-item">
+        2
+      </div>
+      <div onMouseDown={addSymbol} class="grid-item">
+        3
+      </div>
+      <div onMouseDown={addSymbol} class="grid-item">
+        4
+      </div>
+      <div onMouseDown={addSymbol} class="grid-item">
+        5
+      </div>
+      <div onMouseDown={addSymbol} class="grid-item">
+        6
+      </div>
+      <div onMouseDown={addSymbol} class="grid-item">
+        7
+      </div>
+      <div onMouseDown={addSymbol} class="grid-item">
+        8
+      </div>
+      <div onMouseDown={addSymbol} class="grid-item">
+        9
+      </div>
     </div>
   );
 };
